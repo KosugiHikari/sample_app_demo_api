3b189d91af8ab5d5ad7e0c0b777865d16e63a3bf
EC2 Default User <amethyst.purple.0709@gmail.com>
2023-05-21 13:02:09 +0000

    [Updata]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_21_093721) do
+ActiveRecord::Schema.define(version: 2023_05_21_125459) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2023_05_21_093721) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
+    t.decimal "score", precision: 5, scale: 3
   end
 
   create_table "tags", force: :cascade do |t|
